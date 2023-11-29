5911440d648f13cf56e4bfe66867bf40895808d1
Alexis Gonzalez <alexis@tete-chercheuse.fr>
2023-11-29 15:29:41 +0100

    add days and month to calendar

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,12 +4,22 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
-const { db } = require('./dist/core/loadCoreModule');
+const { ipcRenderer } = require('electron');
+
+ipcRenderer.on('loadCalendar', (event, currentDate) => {
+  const { year, month } = currentDate;
+
+
+const { calendar } = require('./dist/features/calendar');
+  calendar.createCalendar(year, month);
+});
+
+ipcRenderer.send('getCurrentDate');
 
 const test = {
-    date_deb: new Date(Date.now()),
-    date_fin: new Date(Date.now()),
-    titre: "test"
+  date_deb: new Date(Date.now()),
+  date_fin: new Date(Date.now()),
+  titre: "test"
 };
 
-db.functions.addEvent(db.knex, 'events', test);
+ipcRenderer.send('addEvent', test);
